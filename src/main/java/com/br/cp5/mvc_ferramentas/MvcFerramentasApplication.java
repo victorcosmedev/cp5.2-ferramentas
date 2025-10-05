cfc5dde1999f31bf2efd51ccd6f8eb49709e4e73
victorcosmedev <victorcosme.profissional@gmail.com>
2025-10-05 16:42:12 -0300

    initial commit

---
+++ src/main/java/com/br/cp5/mvc_ferramentas/MvcFerramentasApplication.java
@@ -0,0 +1,13 @@
+package com.br.cp5.mvc_ferramentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFerramentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcFerramentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/cp5/mvc_ferramentas/MvcFerramentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cp5.mvc_ferramentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFerramentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
